4e51ddb4939e4ef18ebd3332f7149c2532b9e535
cgaguilar <cgaguilar@gmail.com>
2012-04-25 04:01:58 +0000

    changed map to be reloading

--- static/js/mapthing.js
+++ static/js/mapthing.js
@@ -14,21 +14,20 @@ function getQueryVariable(variable) {
 } 
 
 function drawPath(path) {
-  //var map = new L.Map('map');
   var cloudmade = new L.TileLayer('http://{s}.tile.cloudmade.com/7ed9bab0587c49f79a34e6c987ed60fb/997/256/{z}/{x}/{y}.png', {
     attribution: '',
   });
-
+  allPathsLayer.clearLayers();
   var latlngs = [];
   $.each(path.points, function (key, val) {
     var point = new L.LatLng(val.lat, val.lon);
     latlngs.push(point);
     var marker = new L.Marker(point);
-    map.addLayer(marker);
+    allPathsLayer.addLayer(marker);
     marker.bindPopup('Point: ' + val.id + ' at Time: ' + val.time);
   });
-  var polyline = new L.Polyline(latlngs, {color: 'blue'});
-  map.addLayer(polyline);
+  var polyline = new L.Polyline(latlngs ); // ,{color: 'blue'}
+  allPathsLayer.addLayer(polyline);
   map.fitBounds(new L.LatLngBounds(latlngs));
   map.addLayer(cloudmade); 
 }
