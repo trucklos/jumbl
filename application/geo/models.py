de5b5f67c62bf3595e2dfbdb80a93d6df48b331a
Nathaniel Stokoe <nstokoe@gmail.com>
2012-05-21 21:57:46 -0400

    db model update for googleid and email

--- application/geo/models.py
+++ application/geo/models.py
@@ -6,7 +6,7 @@ class User(models.Model):
    email = models.CharField(max_length=255)
    googleid = models.CharField(max_length=255, unique = True)
    def __unicode__(self):
-       return u'%s' % (self.username, self.email, self.googleid)
+       return u'%s' % (self.username)
 
 class Path(models.Model):
    user = models.ForeignKey(User, related_name='paths')
