08de64158b29969507bfb4ec7eabfa0d5e514623
Danilo Duran <dannyd933@gmail.com>
2012-06-06 02:10:15 -0400

    first-time use triggers email for account provisioning

--- application/geo/views.py
+++ application/geo/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+import mapthing.settings
+
+def send_email(request):
+    gid = request.REQUEST.get('gid', '')
+    name = request.REQUEST.get('name', '')
+    email = request.REQUEST.get('email', '')
+    picture = request.REQUEST.get('picture', '')
+
+    subject = 'registration: ' + name + " " + gid
+    message = 'name: ' + name + '\ngid: ' + gid + '\nemail: ' + email + '\npicture: ' + picture
+    from_email = 'registration@jumbl.us'
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, mapthing.settings.registration_email_recipients)
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('../approval.html')
+    else:
+        # In reality we'd use a form class
+        # to get proper validation errors.
+        return HttpResponse('Make sure all fields are entered and valid.')
+
--- application/mapthing/urls.py
+++ application/mapthing/urls.py
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
   url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
   url(r'^admin/', include(admin.site.urls)),
   url(r'^api/', include('api.urls')), 
+  url(r'^registration', 'geo.views.send_email'), 
 )
