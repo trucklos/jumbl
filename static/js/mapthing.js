b02e715d966a676f6790f4744c8e9ec9b73a0575
Nathaniel Stokoe <nstokoe@gmail.com>
2012-06-15 00:21:09 +0000

    mapthing js change

--- static/js/mapthing.js
+++ static/js/mapthing.js
@@ -224,8 +224,7 @@ mt.loadUserPathList = function(pathList) {
         $('select#jQuery-path-select-list').empty();
         
         $('select#pathSelectList').append(pathSelectItems.join('\n') ); 
-        $('select#jQuery-path-select-list').append(pathSelectItems.join('\n') );
-        $('select#jQuery-path-select-list option').first().attr("selected", true);   
+        $('select#jQuery-path-select-list').append(pathSelectItems.join('\n') );      
 
         if(mobile){
           $('select#pathSelectList').trigger("change");
